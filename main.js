34f1bbd64c358136a61eff6f173a6efd92450f97
Yuki-jukjis <taka0618.m@gmail.com>
2020-04-14 23:56:38 +0900

    スクリーン描画テスト

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 "use strict";
+window.onload = () => {
+    // HTMLにあるcanvasを持ってくる
+    let canvas = document.getElementById("canvas");
+    if (canvas === null || !(canvas instanceof HTMLCanvasElement))
+        return;
+    let context = canvas.getContext("2d");
+    if (context === null)
+        return;
+    // スクリーンになるcanvasを作成
+    let screenCanvas = document.createElement("canvas");
+    let screenContext = screenCanvas.getContext("2d");
+    if (screenContext === null)
+        return;
+    // スクリーンに描画
+    screenContext.fillStyle = "red";
+    screenContext.fillRect(50, 50, 30, 30);
+    // メインのcanvasに描画
+    context.drawImage(screenCanvas, 0, 0);
+};
---
+++ main.ts
@@ -0,0 +1,19 @@
+window.onload = () => {
+  // HTMLにあるcanvasを持ってくる
+  let canvas = document.getElementById("canvas");
+  if (canvas === null || !(canvas instanceof HTMLCanvasElement)) return;
+  let context = canvas.getContext("2d");
+  if (context === null) return;
+
+  // スクリーンになるcanvasを作成
+  let screenCanvas: HTMLCanvasElement = document.createElement("canvas");
+  let screenContext = screenCanvas.getContext("2d");
+  if (screenContext === null) return;
+
+  // スクリーンに描画
+  screenContext.fillStyle = "red";
+  screenContext.fillRect(50, 50, 30, 30);
+
+  // メインのcanvasに描画
+  context.drawImage(screenCanvas, 0, 0);
+}
