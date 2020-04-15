b306250389d275ae94091400aa9fd07c5911c98e
Yuki-jukjis <taka0618.m@gmail.com>
2020-04-15 17:05:17 +0900

    シェーダーを使ったマルチテクスチャレンダリング

--- main.js
+++ main.js
@@ -37,30 +37,233 @@ function imageLoader(sources, callback = () => { }, progress = {
     return progress;
 }
 // ただの（アニメーションしない）テクスチャを作る
-function texture(source, offsetX, offsetY) {
+function createStaticTexture(source, offsetX, offsetY) {
     return {
-        draw: (x, y, context, resources) => {
-            const img = resources.get(source);
-            if (img === undefined) {
+        draw: (x, y, layers, resources) => {
+            const image = resources.get(source);
+            if (image === undefined) {
                 console.log("not loaded yet");
                 return;
             }
-            context.drawImage(img, offsetX + x, offsetY + y);
+            layers[0].drawImage(image, offsetX + x, offsetY + y);
+        }
+    };
+}
+function createStaticVolumeTexture(source, offsetX, offsetY, sh) {
+    return {
+        draw: (x, y, layers, resources) => {
+            const image = resources.get(source);
+            if (image === undefined) {
+                console.log("not loaded yet");
+                return;
+            }
+            for (var i = 0; i < layerNum; i++)
+                layers[i].drawImage(image, 0, i * sh, image.width, sh, offsetX + x, offsetY + y, image.width, sh);
         }
     };
 }
 // 使いたい画像を配列で指定してロードにかける。ロードが終わったときに呼ぶ関数を指定しとく。第三引数は以前のLoadingProgressに引き続きロードしたい時に使う。
-let imageLoadingProgress = imageLoader(["texture.png", "bar.png"], () => { });
+let imageLoadingProgress = imageLoader(["volumeTest.png", "texture.png"], draw);
 // sourceをID代わりにしてコンストラクタに指定
-let tex0 = texture("texture.png", 0, 0);
+let tex0 = createStaticVolumeTexture("volumeTest.png", 0, 0, 32);
+const layerNum = 8;
+const layerX = 256;
+const layerY = 256;
+const composeX = 200;
+const composeY = 200;
 function draw() {
-    // HTMLにあるcanvasを持ってくる
-    let canvas = document.getElementById("canvas");
-    if (canvas === null || !(canvas instanceof HTMLCanvasElement))
-        return;
-    let context = canvas.getContext("2d");
-    if (context === null)
-        return;
-    // こんな感じで呼び出す？
-    tex0.draw(3, 5, context, imageLoadingProgress.loadedImage);
-}
+    const canvas = (() => {
+        const x = document.getElementById("canvas");
+        if (x === null || !(x instanceof HTMLCanvasElement))
+            throw new Error("canvas not found");
+        return x;
+    })();
+    const context = (() => {
+        const x = canvas.getContext("2d");
+        if (x === null)
+            throw new Error("context2d not found");
+        return x;
+    })();
+    context.imageSmoothingEnabled = false;
+    const layers = [];
+    for (var i = 0; i < layerNum; i++)
+        layers.push(create2dScreen(layerX, layerY));
+    const vssource = `
+  attribute vec3 position;
+  attribute vec2 textureCoord;
+
+  varying   vec2 vTextureCoord;
+
+  void main(void){
+    vTextureCoord = textureCoord;
+    gl_Position   = vec4(position, 1.0);
+  }`;
+    const fssource = `
+  precision mediump float;
+
+  uniform sampler2D texture0;
+  uniform sampler2D texture1;
+
+  varying vec2      vTextureCoord;
+
+  void main(void){
+      vec4 smpColor0 = texture2D(texture0, vTextureCoord);
+      vec4 smpColor1 = texture2D(texture1, vTextureCoord);
+      gl_FragColor  = smpColor0 + smpColor1;
+  }`;
+    const polygon = {
+        position: [
+            -1.0, 1.0,
+            1.0, 1.0,
+            -1.0, -1.0,
+            1.0, -1.0,
+        ],
+        textureCoord: [
+            (1 - composeX / layerX) / 2, (1 - composeY / layerY) / 2,
+            (1 + composeX / layerX) / 2, (1 - composeY / layerY) / 2,
+            (1 - composeX / layerX) / 2, (1 + composeY / layerY) / 2,
+            (1 + composeX / layerX) / 2, (1 + composeY / layerY) / 2
+        ],
+        index: [
+            0, 1, 2,
+            3, 2, 1
+        ]
+    };
+    const gl = createGlScreen(composeX, composeY);
+    const prg = createProgram(createVertexShader(vssource), createFragmentShader(fssource));
+    // VBOとIBOの登録
+    setAttribute(createVbo(polygon.position), gl.getAttribLocation(prg, 'position'), 2);
+    setAttribute(createVbo(polygon.textureCoord), gl.getAttribLocation(prg, 'textureCoord'), 2);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, createIbo(polygon.index));
+    for (let i = 0; i < layerNum; i++)
+        gl.uniform1i(gl.getUniformLocation(prg, 'texture' + i), i);
+    for (let i = 0; i < layerNum; i++)
+        document.body.appendChild(layers[i].canvas);
+    document.body.appendChild(gl.canvas);
+    loop();
+    function loop() {
+        tex0.draw(100, 100, layers, imageLoadingProgress.loadedImage);
+        /*
+        layers[0].fillStyle = "red";
+        layers[0].fillRect(100, 100, 200, 200);
+        layers[1].clearRect(0, 0, 256, 256);
+        layers[1].fillStyle = "blue";
+        layers[1].fillRect(Math.random() * 300, Math.random() * 300, Math.random() * 200, Math.random() * 200);
+        */
+        composit();
+        requestAnimationFrame(loop);
+    }
+    function composit() {
+        // canvasを初期化
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearDepth(1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        const textures = [];
+        for (let i = 0; i < layerNum; i++)
+            textures[i] = attachImage(layers[i].canvas, i);
+        gl.drawElements(gl.TRIANGLES, polygon.index.length, gl.UNSIGNED_SHORT, 0);
+        textures.forEach(t => gl.deleteTexture(t));
+        gl.flush();
+        context.clearRect(0, 0, 400, 400);
+        context.drawImage(gl.canvas, 0, 0, 400, 400);
+    }
+    function create2dScreen(width, height) {
+        let canvas = document.createElement("canvas");
+        canvas.width = width;
+        canvas.height = height;
+        let context = canvas.getContext("2d");
+        if (context === null)
+            throw new Error("failed to get 2D context");
+        return context;
+    }
+    function createGlScreen(width, height) {
+        const canvas = document.createElement("canvas");
+        canvas.width = width;
+        canvas.height = height;
+        // webglコンテキストを取得
+        const context = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+        if (context === null || !(context instanceof WebGLRenderingContext))
+            throw new Error("failed to get GL context");
+        return context;
+    }
+    // VBOをバインドし登録する関数
+    function setAttribute(vbo, location, attS) {
+        // バッファをバインドする
+        gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+        // attributeLocationを有効にする
+        gl.enableVertexAttribArray(location);
+        // attributeLocationを通知し登録する
+        gl.vertexAttribPointer(location, attS, gl.FLOAT, false, 0, 0);
+    }
+    // 頂点シェーダの生成
+    function createVertexShader(source) {
+        const vs = gl.createShader(gl.VERTEX_SHADER);
+        if (vs === null)
+            throw new Error("failed to create v-shader");
+        gl.shaderSource(vs, source);
+        gl.compileShader(vs);
+        if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
+            console.log(gl.getShaderInfoLog(vs));
+            throw new Error("failed to compile v-shader");
+        }
+        return vs;
+    }
+    function createFragmentShader(source) {
+        const fs = gl.createShader(gl.FRAGMENT_SHADER);
+        if (fs === null)
+            throw new Error("failed to create f-shader");
+        gl.shaderSource(fs, source);
+        gl.compileShader(fs);
+        if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
+            console.log(gl.getShaderInfoLog(fs));
+            throw new Error("failed to compile f-shader");
+        }
+        return fs;
+    }
+    // プログラムオブジェクトの生成
+    function createProgram(vs, fs) {
+        const prg = gl.createProgram();
+        if (prg === null)
+            throw new Error("failed to create shader program");
+        gl.attachShader(prg, vs);
+        gl.attachShader(prg, fs);
+        gl.linkProgram(prg);
+        if (!gl.getProgramParameter(prg, gl.LINK_STATUS)) {
+            console.log(gl.getProgramInfoLog(prg));
+            throw new Error("failed to link shader program");
+        }
+        gl.useProgram(prg);
+        return prg;
+    }
+    // VBOを生成する関数
+    function createVbo(data) {
+        var vbo = gl.createBuffer();
+        if (vbo === null)
+            throw new Error("failed to create VBO");
+        gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        return vbo;
+    }
+    // IBOを生成する関数
+    function createIbo(data) {
+        var ibo = gl.createBuffer();
+        if (ibo === null)
+            throw new Error("failed to create IBO");
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+        return ibo;
+    }
+    // テクスチャを生成する関数
+    function attachImage(image, index) {
+        const texture = gl.createTexture();
+        if (texture === null)
+            throw new Error("failed to create texture");
+        gl.activeTexture(gl.TEXTURE0 + index);
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        gl.generateMipmap(gl.TEXTURE_2D);
+        return texture;
+    }
+} //*/
--- main.ts
+++ main.ts
@@ -196,10 +196,10 @@ onload = function(){
   // テクスチャを生成する関数
   function texture(source: string){
       // イメージオブジェクトの生成
-      var img = new Image();
+      var image = new Image();
 
       // データのオンロードをトリガーにする
-      img.onload = function(){
+      image.onload = function(){
           // テクスチャオブジェクトの生成
           tex = gl.createTexture();
           
@@ -207,7 +207,7 @@ onload = function(){
           gl.bindTexture(gl.TEXTURE_2D, tex);
           
           // テクスチャへイメージを適用
-          gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+          gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
           
           // ミップマップを生成
           gl.generateMipmap(gl.TEXTURE_2D);
@@ -217,11 +217,12 @@ onload = function(){
       };
       
       // イメージオブジェクトのソースを指定
-      img.src = source;
+      image.src = source;
   }
   
-};*/
+};//*/
 
+//*
 type ImageResources = Map<string, HTMLImageElement>;
 interface ImageLoadingProgress {
   finishedCount: number;
@@ -251,33 +252,250 @@ function imageLoader(sources: string[], callback: ()=>void = () => { }, progress
 
 interface Texture {
   // これの実装を色々にしてアニメーションなどを表現する
-  draw: (x:number, y:number, context:CanvasRenderingContext2D, resources:ImageResources) => void;
+  draw: (x:number, y:number, contexts:CanvasRenderingContext2D[], resources:ImageResources) => void;
 }
 
 // ただの（アニメーションしない）テクスチャを作る
-function texture(source: string, offsetX: number, offsetY: number): Texture{
+function createStaticTexture(source: string, offsetX: number, offsetY: number): Texture{
   return {
-    draw: (x:number, y:number, context:CanvasRenderingContext2D, resources:ImageResources) => {
-      const img = resources.get(source);
-      if (img === undefined) { console.log("not loaded yet"); return; }
-      context.drawImage(img, offsetX + x, offsetY + y)
+    draw: (x:number, y:number, layers:CanvasRenderingContext2D[], resources:ImageResources) => {
+      const image = resources.get(source);
+      if (image === undefined) { console.log("not loaded yet"); return; }
+        layers[0].drawImage(image, offsetX + x, offsetY + y)
+    }
+  }
+}
+
+function createStaticVolumeTexture(source: string, offsetX:number, offsetY:number, sh:number): Texture {
+  return {
+    draw: (x:number, y:number, layers:CanvasRenderingContext2D[], resources:ImageResources) => {
+      const image = resources.get(source);
+      if (image === undefined) { console.log("not loaded yet"); return; }
+      for(var i = 0; i < layerNum; i++)
+        layers[i].drawImage(image, 0, i * sh, image.width, sh, offsetX + x, offsetY + y, image.width, sh);
     }
   }
 }
 
 // 使いたい画像を配列で指定してロードにかける。ロードが終わったときに呼ぶ関数を指定しとく。第三引数は以前のLoadingProgressに引き続きロードしたい時に使う。
-let imageLoadingProgress = imageLoader(["texture.png", "bar.png"], () => { /* ゲーム開始処理 */});
-
+let imageLoadingProgress = imageLoader(["volumeTest.png", "texture.png"], draw);
 // sourceをID代わりにしてコンストラクタに指定
-let tex0 = texture("texture.png", 0, 0);
+let tex0 = createStaticVolumeTexture("volumeTest.png", 0, 0, 32);
 
-function draw() {
-  // HTMLにあるcanvasを持ってくる
-  let canvas = document.getElementById("canvas");
-  if (canvas === null || !(canvas instanceof HTMLCanvasElement)) return;
-  let context = canvas.getContext("2d");
-  if (context === null) return;
+const layerNum = 8;
+const layerX = 256;
+const layerY = 256;
+const composeX = 200;
+const composeY = 200;
+
+
+function draw(){
+  const canvas: HTMLCanvasElement = (() => {
+    const x = document.getElementById("canvas");
+    if　(x === null || !(x instanceof HTMLCanvasElement))
+      throw new Error("canvas not found");
+    return x;
+  })();
+  const context: CanvasRenderingContext2D = (() => {
+    const x = canvas.getContext("2d");
+    if　(x === null)
+      throw new Error("context2d not found");
+    return x;
+  })();
+  context.imageSmoothingEnabled = false;
+
+  const layers: CanvasRenderingContext2D[] = [];
+  for(var i = 0; i < layerNum; i++) 
+    layers.push(create2dScreen(layerX, layerY));
+
+  const vssource:string = `
+  attribute vec3 position;
+  attribute vec2 textureCoord;
+
+  varying   vec2 vTextureCoord;
+
+  void main(void){
+    vTextureCoord = textureCoord;
+    gl_Position   = vec4(position, 1.0);
+  }`;
+  const fssource:string = `
+  precision mediump float;
+
+  uniform sampler2D texture0;
+  uniform sampler2D texture1;
+
+  varying vec2      vTextureCoord;
+
+  void main(void){
+      vec4 smpColor0 = texture2D(texture0, vTextureCoord);
+      vec4 smpColor1 = texture2D(texture1, vTextureCoord);
+      gl_FragColor  = smpColor0 + smpColor1;
+  }`;
+  const polygon = {
+  position: [
+    -1.0,  1.0,
+     1.0,  1.0,
+    -1.0, -1.0,
+     1.0, -1.0,
+  ],
+  textureCoord: [
+    (1 - composeX / layerX) / 2, (1 - composeY / layerY) / 2,
+    (1 + composeX / layerX) / 2, (1 - composeY / layerY) / 2,
+    (1 - composeX / layerX) / 2, (1 + composeY / layerY) / 2,
+    (1 + composeX / layerX) / 2, (1 + composeY / layerY) / 2
+  ],
+  index: [
+    0, 1, 2,
+    3, 2, 1
+  ]};
+
+  const gl = createGlScreen(composeX, composeY);
+  const prg = createProgram(createVertexShader(vssource), createFragmentShader(fssource));
+
+  // VBOとIBOの登録
+  setAttribute(createVbo(polygon.position), gl.getAttribLocation(prg, 'position'), 2);
+  setAttribute(createVbo(polygon.textureCoord), gl.getAttribLocation(prg, 'textureCoord'), 2);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, createIbo(polygon.index));
+
+  for(let i = 0; i < layerNum; i++)
+    gl.uniform1i(gl.getUniformLocation(prg, 'texture' + i), i);
   
-  // こんな感じで呼び出す？
-  tex0.draw(3, 5, context, imageLoadingProgress.loadedImage);
-}
+  for(let i = 0; i < layerNum; i++)
+    document.body.appendChild(layers[i].canvas);
+    document.body.appendChild(gl.canvas);
+  loop();
+
+  function loop(): void {
+    tex0.draw(100, 100, layers, imageLoadingProgress.loadedImage);
+    /*
+    layers[0].fillStyle = "red";
+    layers[0].fillRect(100, 100, 200, 200);
+    layers[1].clearRect(0, 0, 256, 256);
+    layers[1].fillStyle = "blue";
+    layers[1].fillRect(Math.random() * 300, Math.random() * 300, Math.random() * 200, Math.random() * 200);
+    */
+    composit();
+    requestAnimationFrame(loop);
+  }
+
+  function composit(): void {
+    // canvasを初期化
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    const textures = [];
+    for(let i = 0; i < layerNum; i++)
+      textures[i] = attachImage(layers[i].canvas, i);
+
+    gl.drawElements(gl.TRIANGLES, polygon.index.length, gl.UNSIGNED_SHORT, 0);
+    
+    textures.forEach(t => gl.deleteTexture(t));
+
+    gl.flush();
+
+    context.clearRect(0, 0, 400, 400);
+    context.drawImage(gl.canvas, 0, 0, 400, 400);
+  }
+  
+  function create2dScreen(width: number, height: number): CanvasRenderingContext2D {
+    let canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    let context = canvas.getContext("2d");
+    if (context === null) throw new Error("failed to get 2D context");
+    return context;
+  }
+
+    
+    function createGlScreen(width: number, height: number): WebGLRenderingContext {
+      const canvas = document.createElement("canvas");
+      canvas.width = width;
+      canvas.height = height;
+      // webglコンテキストを取得
+      const context = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+      if (context === null || !(context instanceof WebGLRenderingContext))
+        throw new Error("failed to get GL context");
+      return context;
+    }
+
+    // VBOをバインドし登録する関数
+    function setAttribute(vbo: WebGLBuffer, location: number, attS: number){
+      // バッファをバインドする
+      gl.bindBuffer(gl.ARRAY_BUFFER, vbo);    
+      // attributeLocationを有効にする
+      gl.enableVertexAttribArray(location);
+      // attributeLocationを通知し登録する
+      gl.vertexAttribPointer(location, attS, gl.FLOAT, false, 0, 0);
+    }
+
+    // 頂点シェーダの生成
+    function createVertexShader(source: string): WebGLShader {
+      const vs = gl.createShader(gl.VERTEX_SHADER);
+      if (vs === null) throw new Error("failed to create v-shader");
+      gl.shaderSource(vs, source);
+      gl.compileShader(vs);
+      if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
+        console.log(gl.getShaderInfoLog(vs));
+        throw new Error("failed to compile v-shader");
+      }
+      return vs;
+    }
+    function createFragmentShader(source: string): WebGLShader {
+      const fs = gl.createShader(gl.FRAGMENT_SHADER);
+      if (fs === null) throw new Error("failed to create f-shader");
+      gl.shaderSource(fs, source);
+      gl.compileShader(fs);
+      if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
+        console.log(gl.getShaderInfoLog(fs))
+        throw new Error("failed to compile f-shader");
+      }
+      return fs;
+    } 
+
+    // プログラムオブジェクトの生成
+    function createProgram(vs: WebGLShader, fs: WebGLShader): WebGLProgram {
+      const prg = gl.createProgram();
+      if (prg === null) throw new Error("failed to create shader program");
+      gl.attachShader(prg, vs);
+      gl.attachShader(prg, fs);
+      gl.linkProgram(prg);      
+      if(!gl.getProgramParameter(prg, gl.LINK_STATUS)){
+        console.log(gl.getProgramInfoLog(prg));
+        throw new Error("failed to link shader program");
+      }
+      gl.useProgram(prg);
+      return prg;
+    }
+
+    // VBOを生成する関数
+    function createVbo(data: number[]): WebGLBuffer{
+        var vbo = gl.createBuffer();
+        if (vbo === null) throw new Error("failed to create VBO");
+        gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        return vbo;
+    }
+
+    // IBOを生成する関数
+    function createIbo(data: number[]): WebGLBuffer {
+        var ibo = gl.createBuffer();
+        if (ibo === null) throw new Error("failed to create IBO");
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+        return ibo;
+    }
+
+    // テクスチャを生成する関数
+    function attachImage(image: HTMLImageElement | HTMLCanvasElement, index: number): WebGLTexture {
+      const texture = gl.createTexture();
+      if (texture === null) throw new Error("failed to create texture");
+      gl.activeTexture(gl.TEXTURE0 + index);
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+      gl.generateMipmap(gl.TEXTURE_2D);
+      return texture;
+  }
+}//*/
